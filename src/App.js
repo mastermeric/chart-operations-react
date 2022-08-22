663a52bf528196c67df81e5abf88de1e76494ba5
mastermeric <mastermeric@gmail.com>
2022-08-23 01:10:54 +0300

    graphical additions..

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import {
   Chart as ChartJS,
+  PointElement,
+  LineElement,
   CategoryScale,
   LinearScale,
   BarElement,
   Title,
   Tooltip,
-  Legend,
+  Legend  
 } from 'chart.js';
-import {Bar} from 'react-chartjs-2';
 
+import {Bar,Line} from 'react-chartjs-2';
 
 ChartJS.register(
   CategoryScale,
   LinearScale,
-  BarElement,
+  BarElement,  
+  PointElement,
+  LineElement,
   Title,
   Tooltip,
-  Legend
+  Legend,  
 );
 
 const state = {
@@ -25,7 +29,7 @@ const state = {
            'April', 'May'],
   datasets: [
     {
-      label: 'Rainfall',
+      label: 'Rain Levels',
       backgroundColor: 'rgba(75,192,192,1)',
       borderColor: 'rgba(0,0,0,1)',
       borderWidth: 2,
@@ -34,15 +38,30 @@ const state = {
   ]
 }
 
+const state2 = {
+  labels: ['January', 'February', 'March',
+           'April', 'May'],
+  datasets: [
+    {
+      label: 'Snow raining Levels',
+      fill: false,
+      lineTension: 0.5,
+      backgroundColor: 'rgba(75,192,192,1)',
+      borderColor: 'rgba(0,0,0,1)',
+      borderWidth: 2,
+      data: [65, 59, 80, 81, 56]
+    }
+  ]
+}
 
 function App() {
-
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>MERHABA</h1>
+        <h1>Grafik-1 : Bar chart</h1>
 
+        <div>
         <Bar
           data={state}
           options={{
@@ -57,7 +76,26 @@ function App() {
             }
           }}
         />
+        </div>
 
+        <h1>Grafik-2 : Line chart</h1>
+
+        <div>
+          <Line
+            data={state2}
+            options={{
+              title:{
+                display:true,
+                text:'Average Rainfall per month',
+                fontSize:20
+              },
+              legend:{
+                display:true,
+                position:'right'
+              }
+            }}
+          />
+        </div>
       </header>
     </div>
   );
